9a4e4e5c55f63c4f2d1a79c027939fa258e45a04
Mario86x <mario86x@gmail.com>
2025-09-19 22:56:06 +0200

    deleted chroma dbs, need to recreate them with normalized vectors

--- various_functions.py
+++ various_functions.py
@@ -2,3 +2,10 @@ import hashlib
 
 def chunk_id_from_text(text: str) -> str:
     return hashlib.md5(text.encode("utf-8")).hexdigest()
+
+
+import numpy as np
+
+def normalize(vec):
+    arr = np.array(vec, dtype=np.float32)
+    return arr / np.linalg.norm(arr)
